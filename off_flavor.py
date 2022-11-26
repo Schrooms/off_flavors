54867082b7eb5249ce7cdab31ab04466a6186430
jeroen van heiningen <jeroen.v.heiningen@gmail.com@>
2022-11-26 17:16:19 +0000

    basic

--- off_flavor.py
+++ off_flavor.py
@@ -1,7 +1,7 @@
 # tool for helping me to remember what off flavour do
 from json import load
-
 from dataclasses import dataclass
+import random
 
 
 @dataclass(frozen=True)
@@ -11,6 +11,27 @@ class OffFlavor:
     causes: str
     doemens_nr: int
 
+
+@dataclass(frozen=True)
+class Question:
+    wrong_options: set[str]
+    off_flavor: OffFlavor
+
+    @property
+    def question(self) -> str:
+        return f'Wat proeft of ruikt naar "{self.off_flavor.description}"'
+
+    def formulate(self) -> str:
+        options: list = list(self.wrong_options)
+        options.append(self.off_flavor.name)
+        random.shuffle(options)
+
+        return f'''{self.question}
+    a: {options[0]}
+    b: {options[1]}
+    c: {options[2]}
+    d: {options[3]}
+        '''
 
 def load_off_flavors() -> set[OffFlavor]:
     with open('off_flavors_data.json', 'r') as file:
@@ -23,3 +44,16 @@ def load_off_flavors() -> set[OffFlavor]:
             doemens_nr=data['nr']
             ) for data in json_data
     }
+
+
+def generate_question(data: set[OffFlavor]) -> Question:
+    listed_data = list(data)
+    off_flavor = random.choice(listed_data)
+    wrong_options = random.sample([l.name for l in listed_data if l != off_flavor] , 3)
+    return Question(wrong_options=set(wrong_options), off_flavor=off_flavor)
+
+
+if __name__ == '__main__':
+    data = load_off_flavors()
+    question = generate_question(data=data)
+    print(question.formulate())
--- tests/test_load_off_flavours.py
+++ tests/test_load_off_flavours.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from off_flavor import OffFlavor, load_off_flavors
+from off_flavor import OffFlavor, Question, generate_question, load_off_flavors
 
 class test_off_flavours(TestCase):
     def test_load(self) -> None:
@@ -11,3 +11,10 @@ class test_off_flavours(TestCase):
             doemens_nr=21)
 
         self.assertIn(off_flavor, data)
+
+    def test_generate_question(self) -> None:
+        data = load_off_flavors()
+        question = generate_question(data)
+        self.assertIsInstance(question, Question)
+        self.assertEqual(len(question.wrong_options), 3)
+        self.assertNotIn(question.off_flavor.name, question.wrong_options)
